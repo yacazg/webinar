c9bf2dc413ac61c4c61cd333f90d979e987d3365
yacazg <121126970+yacazg@users.noreply.github.com>
2022-12-20 19:30:32 +0000

    Deploy

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-
-const nextConfig = {
-  reactStrictMode: true,
-  basePath: process.env.GITHUB_REPO ? `/${process.env.GITHUB_REPO}` : "",
-}
-
-module.exports = nextConfig
